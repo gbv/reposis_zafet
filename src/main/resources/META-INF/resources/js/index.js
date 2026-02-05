d8c9172a32ad7f7e00da27f41d43fbde3b54b841
Lennard Golsch <lennard.golsch@icloud.com>
2026-02-05 09:42:12 +0100

    improve index.xml

--- src/main/resources/META-INF/resources/js/index.js
+++ src/main/resources/META-INF/resources/js/index.js
@@ -1,17 +1,30 @@
-document.addEventListener('DOMContentLoaded', () => {
-  document.getElementById('project-searchMainPage')?.addEventListener('submit', ignoreEmptyFieldsOnSubmit);
+async function getTranslation(locale, name) {
+  const response = await fetch(`../rsc/locale/translate/${locale}/${name}`);
+  if (!response.ok) {
+    throw new Error(`HTTP error ${response.status}`);
+  }
+  return await response.text();
+}
 
-  fetch('../api/v1/search?q=objectType:mods AND state:published&rows=0&wt=json')
-    .then(response => response.json())
-    .then(data => {
-      const input = document.getElementById('project-searchInput');
-      if (!input) {
-        return;
-      }
-      const count = data?.response?.numFound ?? 0;
-      input.placeholder = `Suche in ${count.toLocaleString('de-DE')} Dokumenten`;
-    })
-    .catch(err => {
-      console.error('Search API Fehler:', err);
-    });
+async function getDocumentCount() {
+  const response = await fetch('../api/v1/search?q=objectType:mods AND state:published&rows=0&wt=json');
+  if (!response.ok) {
+    throw new Error(`HTTP error ${response.status}`);
+  }
+  const data = await response.json();
+  return data?.response?.numFound;
+}
+
+document.addEventListener('DOMContentLoaded', async () => {
+  document.getElementById('project-searchMainPage')?.addEventListener('submit', ignoreEmptyFieldsOnSubmit);
+  const input = document.getElementById('project-searchInput');
+  if (input) {
+    try {
+      const placeholder = await getTranslation(currentLang, 'project.index.search.placeholder');
+      const count = await getDocumentCount();
+      input.placeholder = placeholder.replace('{0}', count.toLocaleString('de-DE'));
+    } catch {
+      console.error('Error updating search placeholder:', err);
+    }
+  }
 });
